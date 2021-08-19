a3b6659c2141e3d4d1b018ca054313de63600fcb
Douglas <douglascoelho31@hotmail.com>
2021-08-19 16:52:53 -0300

    alteracoes

--- fundamentos/bloco-5-JavaScript-DOM/dia-3/exercicio.js
+++ fundamentos/bloco-5-JavaScript-DOM/dia-3/exercicio.js
@@ -14,3 +14,16 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  function daysList() {
+
+  }
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const liElement0 = document.createElement('li');
+const classNameLi0 = liElement.className = 'nome';
+const textLi = liElement0.innerText = 'sextou';
+const days = document.getElementById('days');
+days.appendChild(liElement0)
+/* adicinei lista pelo JS via DOM*/
+const liElement1 = document.createElement('li');
+const classNameLi1
