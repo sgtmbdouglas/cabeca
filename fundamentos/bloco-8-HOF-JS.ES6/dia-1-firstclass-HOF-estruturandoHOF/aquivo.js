a9c19629019dc1bc2268d0b52d6c69e3fde2e573
Douglas <douglascoelho31@hotmail.com>
2021-09-14 17:08:28 -0300

    arquivos

---
+++ fundamentos/bloco-8-HOF-JS.ES6/dia-1-firstclass-HOF-estruturandoHOF/aquivo.js
@@ -0,0 +1,43 @@
+const sayHello = () => {
+  return ('hello trybers');
+}
+
+const printGreeting = (callback) => {
+    console.log(callback());
+}
+
+printGreeting(sayHello);
+
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+repeat(5, console.group);
+//
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+};
+
+const isOdd = (number) => {
+  if ((number % 2) > 0) {
+    console.log(number, 'is odd');
+  }
+};
+
+repeat(3, isEven); // Testa quais números serão pares;
+repeat(3, isOdd); // Testa quais números serão ímpares;
+
+const numberGenerator = () => {
+  return Math.random() * 100;
+}
+
+console.log(numberGenerator());
