28850440fc1c34ec90e51d50b02886e0c237f4af
Douglas <douglascoelho31@hotmail.com>
2021-11-03 18:47:17 -0300

    comitao bomba

--- front-end/bloco-11-pt2-estado-eventos/dia-1-estado-eventos/mozov/src/App.js
+++ front-end/bloco-11-pt2-estado-eventos/dia-1-estado-eventos/mozov/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './novo'
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+    <Greeting name="Samuel" />
         <a
           className="App-link"
           href="https://reactjs.org"
