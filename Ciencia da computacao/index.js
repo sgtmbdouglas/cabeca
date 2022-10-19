56d0480e89a677b8532a96175aeee464861b37e4
DCBB <dc@pop-os.localdomain>
2022-10-19 15:08:02 -0300

    intro Python

---
+++ Ciencia da computacao/index.js
@@ -0,0 +1,19 @@
+// AJUSTANDO LINT, FORMATACAO, e CRIANDO AMBIENTE VIRTUAL ANTES DE TUDO BOA PRATICA VQV
+// $ sudo apt install python3
+// pip permite intalar as dependencias do python, libs tipo o linter
+// $ sudo apt install python3-pip
+// $ sudo apt install python3.10-venv
+// para criar um ambiente virtual( ambiente usado para desenvolver um determinado projeto, e nele que intalo as libs para o projeto, boa pratica)
+// $ python3 -m venv .venv
+// toda vez que rodo o -m estou executando aquela lib como um arquivo unico, venv(virtual envailvilmente) e o .venv e o nome do meu ambiente virtual uma boa pratica
+// se eu quiser ativar meu ambiente virtual
+// $ source .venv/bin/activate
+// o terminal ficara assim: (.venv) user:$
+// $ pip install flake8
+// famoso linter flake8
+// se der problema na instalacao do flake8 ou para criar um ambiente virtual isso pode acontecer por conta da biblioteca whell, para instalar $pip install wheel
+// $ pip install black
+// ajuda na formatacao do codigo
+// para usar o linter(flake8), ctrl + shift + p e procura Python: Select Linter escolhe a opcao flake8
+// para usar o black eu chamo o arquivo no terminal e passo o black
+// python3 -m black teste_black.py
---
+++ Ciencia da computacao/index2.js
@@ -0,0 +1,60 @@
+//    COMECANDO PYTHON
+
+// As distribuições do sistema operacional Linux e Mac, normalmente, já vem com uma versão Python instalada, pois utilizam a linguagem em diversos programas essenciais
+// Estou usando arquivos js porque python nao aceita comentarios muito grandes
+// Para ver se o python ja esta instalado no terminal digita $ python3
+// Este tipo de terminal é chamado de REPL (Read-Eval-Print Loop), sendo traduzido para o português como loop de leitura-avaliação-impressão ou terminal interativo. Esse terminal recebe uma entrada digitada pela pessoa, avalia sua execução e imprime seu resultado
+// usando o terminal interativo vamos ver a filosofia da linguagem, por meio dela podemos entender um pouco mais sobre como o Python foi escrito.
+// $ import this
+//      PYTHON COM DOCKER
+// # Baixe a última versão do python
+// $ docker pull python
+// $ docker run -it --rm python:tag
+//      OPERADORES ARITIMETICOS
+// >>> 3 // 2               retorna o Quociente da divisao sem o resto
+// >>> 2 ** 10              potenciacao
+//      OPERADORES DE ATRIBUICAO
+// >>> counter = 10             
+// >>> counter +=1              somei counter +1
+//      OPERADORES DE COMPARACAO
+// >>> 10 < 18
+// >>> 18 < 25
+// >>> 10 < 18 < 25             dezoito esta entre 10 e 25?
+//      OPERADORES DE CONDICIONAIS
+// >>> True and False               &&JS
+// >>> True or False                ||JS
+//      TIPOS DE DADOS EMBUTIDOS
+// Os valores booleanos True e False pertencem ao tipo embutido bool.
+// O primeiro dos tipos numéricos é o int
+// O método type(operando)              typeof operando do JavaScript
+// para ajudar a entender algum comando o python dispoe do help 
+// >>> help(list)
+// O comando help() também pode ser utilizado em cláusulas if ou for, desde que colocado entre aspas e para sair de dentro do comando, basta apertar a tecla q.
+// >>> help("if")
+// >>> 
+// >>> 
+// >>> 
+// >>> 
+// >>> 
+// 
+// Um = é atribuição 2 “=“ é comparação
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+//
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
